dee4e66226235d07688adcddecd5f76c6318ebe5
DeanMave <dean.tarantul@gmail.com>
2025-10-29 21:17:54 +0300

    Spring Kafka заменён на стандартный клиент kafka-clients

--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/service/TelemetryKafkaProducer.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/service/TelemetryKafkaProducer.java
@@ -1,41 +1,74 @@
 package ru.yandex.practicum.telemetry.collector.service;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.kafka.serializer.GeneralAvroSerializer;
 import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
 
+import java.util.Properties;
+
 @Service
 public class TelemetryKafkaProducer {
-    private final KafkaTemplate<String, SpecificRecordBase> kafkaTemplate;
+    private KafkaProducer<String, SpecificRecordBase> producer;
 
-    private final String hubTopic;
-    private final String sensorTopic;
+    @Value("${kafka.topic.hub}")
+    private String hubTopic;
+    @Value("${kafka.topic.sensor}")
+    private String sensorTopic;
+    @Value("${kafka.bootstrap-servers}")
+    private String bootstrap;
+    @Value("${kafka.producer.acks}")
+    private String acksConfig;
+    @Value("${kafka.producer.retries}")
+    private String retriesConfig;
+    @Value("${kafka.producer.max-in-flight-requests-per-connection}")
+    private String maxInFlightRequests;
+    @Value("${kafka.producer.linger-ms}")
+    private String lingerMsConfig;
+    @Value("${kafka.producer.batch-size}")
+    private String batchSizeConfig;
 
+    @PostConstruct
+    public void init() {
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrap);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GeneralAvroSerializer.class.getName());
 
-    public TelemetryKafkaProducer(
-            KafkaTemplate<String, SpecificRecordBase> kafkaTemplate,
-            @Value("${kafka.topic.hub}") String hubTopic,
-            @Value("${kafka.topic.sensor}") String sensorTopic
-    ) {
-        this.kafkaTemplate = kafkaTemplate;
-        this.hubTopic = hubTopic;
-        this.sensorTopic = sensorTopic;
+        properties.put(ProducerConfig.ACKS_CONFIG, acksConfig);
+        properties.put(ProducerConfig.RETRIES_CONFIG, retriesConfig);
+        properties.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, maxInFlightRequests);
+
+        properties.put(ProducerConfig.LINGER_MS_CONFIG, lingerMsConfig);
+        properties.put(ProducerConfig.BATCH_SIZE_CONFIG, batchSizeConfig);
+        this.producer = new KafkaProducer<>(properties);
     }
 
-    public void sendSensorEvent(SensorEventAvro event) {
-        String key = event.getHubId();
-
-        kafkaTemplate.send(sensorTopic, key, event);
-
+    @PreDestroy
+    public void close() {
+        if (producer != null) {
+            producer.close();
+        }
     }
 
     public void sendHubEvent(HubEventAvro event) {
         String key = event.getHubId();
+        ProducerRecord<String, SpecificRecordBase> record = new ProducerRecord<>(hubTopic, key, event);
+        producer.send(record);
+    }
 
-        kafkaTemplate.send(hubTopic, key, event);
+    public void sendSensorEvent(SensorEventAvro event) {
+        String key = event.getHubId();
+        ProducerRecord<String, SpecificRecordBase> record = new ProducerRecord<>(sensorTopic, key, event);
+        producer.send(record);
     }
 }
